6396bdd3dcee27340d2807ef222839b6f93f204c
kamilsmr <kamils6035@gmail.com>
2022-10-28 23:58:02 +0300

    uptade

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import Homepage from './Components/Homepage';
+import React from "react";
+import { useSelector } from "react-redux";
+import Blogs from "./Components/Blogs";
+import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
+import { selectSignedIn } from "./features/userSlice";
+import "./styling/app.css";
 
-function App() {
+const App = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Navbar />
+      <Homepage />
+      {isSignedIn && <Blogs />}
     </div>
   );
-}
+};
 
 export default App;
